1a4594745749bdfba778afc3fc3e2456519ce24d
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-10-07 18:23:46 -0500

    Added validation files.

---
+++ src/api/v1/validation/taskValidation.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+
+export const taskSchemas = {
+    create: {
+        body: Joi.object({
+            userId: Joi.string().required().messages({
+                "any.required": "User id is required",
+                "string.empty": "User id cannot be empty",
+            }),
+            title: Joi.string().required().messages({
+                "any.required": "Title is required",
+                "string.empty": "Title cannot be empty",
+            }),
+            priority: Joi.string().valid("low" , "medium" , "high").required().messages({
+                "any.required": "Priority is required",
+                "any.only": "Priority must be one of 'low', 'medium', or 'high'",
+            }),
+            status: Joi.string().valid("open", "in-progress", "completed").required().messages({
+                "any.required": "Status is required",
+                "any.only": "Status must be one of 'open', 'in-progress', or 'completed'",
+            }),
+            dueDate: Joi.date().required().messages({
+                "any.required": "Due date is required",
+                "date.base": "Due date must be a valid date",
+            }),
+        }),
+    }};
