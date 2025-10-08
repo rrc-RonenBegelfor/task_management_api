e65f8c1bff5e13eccb7cbc2051581ca373cb9f9c
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-07 19:10:34 -0500

    Attempt to push changes.

--- src/api/v1/controllers/taskController.ts
+++ src/api/v1/controllers/taskController.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 import * as taskService from "../services/taskService";
-import { successResponse } from "../models/responseModel";
-import { taskSchemas } from "../validation/taskValidation";
 import { Task } from "../models/taskModel";
 
 export const createTask = async (
@@ -11,24 +9,14 @@ res: Response,
 next: NextFunction,
 ): Promise<void> => {
     try {
+        const { userId, title, priority, status, dueDate} = req.body;
 
-        const { error, value } = taskSchemas.create.body.validate(req.body, { abortEarly: false});
+        const newTask: Task = await taskService.createTask({ userId, title, priority, status, dueDate});
 
-        if (error) {
-            res.status(400).json({
-            message: "Validation failed",
-            details: error.details.map(d => d.message),
-          });
-
-          return;
-        }
-
-        const task: Task = value;
-
-        await taskService.createTask({ id: req.params.id, ...task });
-        res.status(200).json(successResponse({}, "Task created"));
-
+        res.status(HTTP_STATUS.CREATED).json({
+            message: "Task created successfully",
+        });
     } catch (error: unknown) {
-        next(error);
+        next(error)
     }
 };
